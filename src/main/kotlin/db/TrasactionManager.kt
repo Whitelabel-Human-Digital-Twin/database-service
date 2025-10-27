7a4548d437427bcd22d13c0a6ada09b2efb50c61
lm98 <leonardomicelli@gmail.com>
2025-10-27 14:26:30 +0100

    feat: implement R2dbc TransactionManager

--- src/main/kotlin/db/TrasactionManager.kt
+++ src/main/kotlin/db/TrasactionManager.kt
@@ -4,6 +4,7 @@ package io.github.whdt.db
 import kotlinx.coroutines.Dispatchers
 import org.jetbrains.exposed.v1.core.Transaction
 import org.jetbrains.exposed.v1.jdbc.transactions.experimental.newSuspendedTransaction
+import org.jetbrains.exposed.v1.r2dbc.transactions.suspendTransaction
 
 interface TransactionManager {
     suspend fun <T> execute(block: suspend Transaction.() -> T): T
@@ -12,5 +13,11 @@ interface TransactionManager {
 object JdbcTransactionManager: TransactionManager {
     override suspend fun <T> execute(block: suspend Transaction.() -> T): T {
         return newSuspendedTransaction(context = Dispatchers.IO, statement = block)
+    }
+}
+
+object R2dbcTransactionManager: TransactionManager {
+    override suspend fun <T> execute(block: suspend Transaction.() -> T): T {
+        return suspendTransaction(statement = block)
     }
 }
