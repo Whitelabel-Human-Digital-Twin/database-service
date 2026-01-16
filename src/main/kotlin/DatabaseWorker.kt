6afb3c02d276e26ce04e0cd99e34f408cc5c3d75
lm98 <leonardomicelli@gmail.com>
2026-01-16 16:51:17 +0100

    refactor: simplify DatabaseWorker message interface

--- src/main/kotlin/DatabaseWorker.kt
+++ src/main/kotlin/DatabaseWorker.kt
@@ -1,37 +1,17 @@
-import db.query.model.AggregateQuery
-import db.query.model.SelectQuery
-import io.github.whdt.core.hdt.model.id.HdtId
-import io.github.whdt.core.hdt.model.property.Property
 import io.ktor.server.application.*
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.launch
 import model.*
-import kotlin.time.ExperimentalTime
-import kotlin.time.Instant
 
-@OptIn(ExperimentalTime::class)
 fun Application.configureDatabaseWorker(commandChannel: Channel<DomainCommand>) {
     launch {
         for (cmd in commandChannel) {
             when (cmd) {
-                is RunSelectQuery -> handleSelectQuery(cmd.query)
-                is RunAggregateQuery -> handleAggregateQuery(cmd.query)
-                is InsertProperty -> handleInsertProperty(cmd.hdt, cmd.property, cmd.receivedAt)
+                is InsertDigitalTwin -> TODO()
+                is InsertDigitalTwinBatch -> TODO()
+                is InsertProperty -> TODO()
                 is NotifyFailureCommand -> log.warn("Failed: ${cmd.reason}")
             }
         }
     }
-}
-
-fun handleSelectQuery(query: SelectQuery) {
-
-}
-
-fun handleAggregateQuery(query: AggregateQuery) {
-
-}
-
-@OptIn(ExperimentalTime::class)
-fun handleInsertProperty(hdt: HdtId, property: Property, receivedAt: Instant) {
-
 }
--- src/main/kotlin/model/DomainCommand.kt
+++ src/main/kotlin/model/DomainCommand.kt
@@ -1,16 +1,21 @@
 package model
 
-import db.query.model.AggregateQuery
-import db.query.model.SelectQuery
 import io.github.whdt.core.hdt.model.id.HdtId
 import io.github.whdt.core.hdt.model.property.Property
+import io.github.whdt.db.entities.HumanDigitalTwin
 import kotlin.time.ExperimentalTime
 import kotlin.time.Instant
 
 sealed interface DomainCommand
 
-data class RunSelectQuery(val query: SelectQuery) : DomainCommand
-data class RunAggregateQuery(val query: AggregateQuery) : DomainCommand
+//data class RunSelectQuery(val query: SelectQuery) : DomainCommand
+//data class RunAggregateQuery(val query: AggregateQuery) : DomainCommand
+
+@OptIn(ExperimentalTime::class)
+data class InsertDigitalTwin(val hdt: HumanDigitalTwin, val receivedAt: Instant) : DomainCommand
+
+@OptIn(ExperimentalTime::class)
+data class InsertDigitalTwinBatch(val hdts: List<HumanDigitalTwin>, val receivedAt: Instant) : DomainCommand
 
 @OptIn(ExperimentalTime::class)
 data class InsertProperty(val hdt: HdtId, val property: Property, val receivedAt: Instant): DomainCommand
